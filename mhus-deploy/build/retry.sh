5e47b88bc7292410de3e1059e8e5ecf479e2ec76
Mike Hummel <>
2019-01-19 14:03:49 +0100

    enhance retry to retry single targets

--- mhus-deploy/build/retry.sh
+++ mhus-deploy/build/retry.sh
@@ -4,4 +4,12 @@ cd /home/user
 
 reset.sh
 
-for i in $(ls deploy/); do echo $i; touch retry/$i; done
+if [  -z $1 ]; then
+  for i in $(ls deploy/); do echo $i; touch retry/$i; done
+else
+  if [  -e deploy/$1 ]; then
+    touch retry/$1
+  else
+    echo "Unknown target $1"
+  fi
+fi
