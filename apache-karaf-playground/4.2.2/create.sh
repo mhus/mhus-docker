dc29422f4fccf91c399bcf850ecebacececbe057
Mike Hummel <>
2019-02-15 15:36:52 +0100

    update karaf containers to run with openjdk 11.0.2

--- apache-karaf-playground/4.2.2/create.sh
+++ apache-karaf-playground/4.2.2/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "clean" ]; then
-	docker build --no-cache -t apache-karaf-playground .
+	docker build --no-cache -t apache-karaf-playground:4.2.2 .
 else
-	docker build -t apache-karaf-playground .
+	docker build -t apache-karaf-playground:4.2.2 .
 fi
--- apache-karaf/4.2.2/create.sh
+++ apache-karaf/4.2.2/create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "clean" ]; then
-	docker build --no-cache -t apache-karaf .
+	docker build --no-cache -t apache-karaf:4.2.2 .
 else
-	docker build -t apache-karaf .
+	docker build -t apache-karaf:4.2.2 .
 fi
