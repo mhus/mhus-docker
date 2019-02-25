b0f6867294ec77f2493a459c2bd9eb3bf21b0902
Mike Hummel <>
2019-02-25 09:07:23 +0100

    add user switch to deploy container

--- mhus-deploy/start.sh
+++ mhus-deploy/start.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+if [ $APP_UID != $(cat /etc/passwd|grep user:|cut -d : -f 3) ]
+then 
+  echo "========================================="
+  echo Need to update UID to $APP_UID
+  echo "========================================="
+  userdel user
+  useradd -u $APP_UID -ms /docker/start.sh user
+  chown -R user:user /home/user
+else 
+  echo UID is $APP_UID;
+fi
+
 export PATH=/home/user/build:$PATH
 
-git config --global credential.helper store
+runuser user git config --global credential.helper store
 
 alias ll='ls -l'
 
 cd /home/user
 chmod +x build/*
-/bin/bash
+exec runuser user /bin/bash $@
