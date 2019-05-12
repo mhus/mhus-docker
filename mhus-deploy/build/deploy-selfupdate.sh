6cdbb93022129fce30b2e1159c2579c0510c1e3f
Mike Hummel <mike@mhus.de>
2019-05-12 21:23:17 +0200

    Update deploy-selfupdate.sh

--- mhus-deploy/build/deploy-selfupdate.sh
+++ mhus-deploy/build/deploy-selfupdate.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-if [ -n -d "/home/user/self" ]; then
+if [ ! -d "/home/user/self" ]; then
   mkdir -p /home/user/self
 fi
 cd /home/user/self
 
-if [ -n -d "mhus-docker" ]; then
+if [ ! -d "mhus-docker" ]; then
   git clone https://github.com/mhus/mhus-docker.git
 fi
 
 for dir in */; do
+  echo "######################################################################"
+  echo "Update $dir"
+  echo "######################################################################"
   cd dir
   if [ -e .git ]; then
     git reset --hard HEAD
     git pull
   fi
   if [ -x deploy-selfupdate.sh ]; then
+    echo "Execute deploy-selfupdate.sh"
     ./deploy-selfupdate.sh
   fi
   cd ..
