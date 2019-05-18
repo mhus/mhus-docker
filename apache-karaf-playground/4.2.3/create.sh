b53e06e4c20155e139342cb63986bcd579b03f8d
Mike Hummel <>
2019-05-18 10:48:40 +0200

    fix scripts

--- apache-karaf-playground/4.2.3/create.sh
+++ apache-karaf-playground/4.2.3/create.sh
@@ -7,8 +7,14 @@ if [  ! -f Dockerfile ]; then
   return 1
 fi
 
+docker rmi mhus/apache-karaf-playground:$VERSION
+
 if [ "$1" = "clean" ]; then
 	docker build --no-cache -t mhus/apache-karaf-playground:$VERSION .
 else
 	docker build -t mhus/apache-karaf-playground:$VERSION .
 fi
+
+if [ "$1" = "push" ]; then
+    docker push "mhus/apache-karaf-playground:$VERSION"
+fi 
--- apache-karaf/4.2.3/create.sh
+++ apache-karaf/4.2.3/create.sh
@@ -7,8 +7,14 @@ if [  ! -f Dockerfile ]; then
   return 1
 fi
 
+docker rmi mhus/apache-karaf:$VERSION
+
 if [ "$1" = "clean" ]; then
 	docker build --no-cache -t mhus/apache-karaf:$VERSION .
 else
 	docker build -t mhus/apache-karaf:$VERSION .
 fi
+
+if [ "$1" = "push" ]; then
+    docker push "mhus/apache-karaf:$VERSION"
+fi 
