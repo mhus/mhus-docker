6e8edcbe7bef4ad240c1887e445426b04ef4bfbc
Mike Hummel <mike.hummel@billingsolutions.de>
2020-01-16 16:36:31 +0100

    Update create.sh

--- elasticstack/7.4.2/logstash/create.sh
+++ elasticstack/7.4.2/logstash/create.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-if [ "$1" = "clean" ]; then
-	docker build --no-cache -t log-logstash .
-else
-	docker build -t log-logstash .
+VERSION=7.4.2
+REPOSITORY=mhus/log-logstash
+
+if [  ! -f Dockerfile ]; then
+  echo "not a docker configuration"
+  return 1
 fi
+
+docker rmi $REPOSITORY:$VERSION
+
+if [ "$1" = "clean" ]; then
+    docker build --no-cache -t $REPOSITORY:$VERSION .
+else
+    docker build -t $REPOSITORY:$VERSION .
+fi
+
+if [ "$1" = "push" ]; then
+    docker push "$REPOSITORY:$VERSION"
+fi 
